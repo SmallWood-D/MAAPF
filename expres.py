135cef7499b104af6b21a0ab5a27152803e2c709
SmallWood-D <shay.gozan@gmail.com>
2019-10-13 15:08:36 +0300

    add new fields to result csv.

--- expres.py
+++ expres.py
@@ -62,7 +62,8 @@ def print_result_csv(file_name, graph, vi, ID, limit, prob_range, start, target,
         print(f"average optimal {opt_avg / len(start)}", file=result_fd)
 
 
-def print_result_json(graph, vi, ID, limit, prob_range, start, target, vi_time, quality, board):
+def print_result_json(graph, vi, ID, limit, prob_range, start, target, vi_time, quality, board, algo, board_id,
+                      heuristic="NA"):
     result = dict()
     # result['VI_table'] = vi.table
     result["time"] = vi_time
@@ -72,12 +73,15 @@ def print_result_json(graph, vi, ID, limit, prob_range, start, target, vi_time, 
     result["probability"] = graph.prob
     result["agents"] = len(next(iter(vi.table.keys())))
     result["ID"] = ID
+    result["algo"] = algo
     result["expected utility"] = vi.table[start][-1]
     result["min range"] = prob_range[0]
     result["max range"] = prob_range[1]
     result["board size"] = f"{len(board[0]) - 2} X {len(board) - 2}"
     result["number of iteration"] = limit if limit else -1
     result["board"] = board
+    result["board_id"] = board_id
+    result["heuristic"] = heuristic
 
     opt_avg = 0
     for s, g in zip(start, target):
@@ -89,10 +93,21 @@ def print_result_json(graph, vi, ID, limit, prob_range, start, target, vi_time, 
 
 
 def collect_res_csv(file_name):
-    columns_titles = ["ID", "time", "quality", "agents", "expected utility", "min range", "max range",
-                      "board size", "number of iteration", "average optimal"]
+    columns_titles = {"ID": "experiments ID",
+                      "time": "algorithm run time",
+                      "quality": "the percentage of success travers of the path - no agent got destory",
+                      "agents": "number of agents",
+                      "expected utility": "the value of the start state.",  # TODO
+                      "min range": "probability minimum range",
+                      "max range": "probability maximum range",
+                      "board size": "board size or board shape",
+                      "number of iteration": "number of iteration the until the algorithm finsih",
+                      "average optimal": "average of the sums of -log(portability) for all agents",
+                      "algo": "name of the algorithm",
+                      "board_id": "board id",
+                      "heuristic": "name of the heuristic fucntion"}
     with open(f"results{os.path.sep}{file_name}", 'w', newline='') as result_fd:
-        writer = csv.DictWriter(result_fd, fieldnames=columns_titles)
+        writer = csv.DictWriter(result_fd, fieldnames=columns_titles.keys())
         writer.writeheader()
         exp_json = glob.glob(f"results{os.path.sep}*.json")
         for exp_f in exp_json:
@@ -100,3 +115,5 @@ def collect_res_csv(file_name):
                 data = json.load(exp_fd)
                 filter_data = {k: data[k] for k in data if k in columns_titles}
                 writer.writerow(filter_data)
+        for c, i in columns_titles.items():
+            print(f"{c}, {i}", file=result_fd)
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from Algo.rtdp import RTDP
 from expres import print_result_json, collect_res_csv, evaluate_policy
 
 if __name__ == '__main__':
-    for i in range(100):
-        ID = f"VI_{i}_{time.time()}"
-        board, prob = gen_board(3, 4, 0.5, 0.8)
+    for i in range(10):
+        local_time = time.localtime()
+        board_id = f"{i}_{local_time.tm_hour}{local_time.tm_min}{local_time.tm_sec}"
+        ID = f"{i}_{local_time.tm_hour}{local_time.tm_min}{local_time.tm_sec}"
+        board, prob = gen_board(3, 4, 0.1, 0.3)
         mdp = Graph(board, prob)
         vi_g = VI(mdp)
         start = time.time()
@@ -17,9 +19,10 @@ if __name__ == '__main__':
         vi_time = end - start
         vi_g.vi_policy(("0|0", "2|3"))
         quality = evaluate_policy(mdp, vi_g, ("1|1", "1|2"), ("0|0", "2|3"), 1000)
-        print_result_json(mdp, vi_g, ID, 4, (0.5, 0.8), ("1|1", "1|2"), ("0|0", "2|3"), vi_time, quality * 100, board)
+        print_result_json(mdp, vi_g, ID, 4, (0.5, 0.8), ("1|1", "1|2"), ("0|0", "2|3"), vi_time, quality * 100,
+                          board, "VI", board_id)
 
-        ID = f"RTDP_{i}_{time.time()}"
+        ID = f"{i}_{local_time.tm_hour}{local_time.tm_min}{local_time.tm_sec}"
         mdp = Graph(board, prob)
         rtdp_g = RTDP(mdp)
         start = time.time()
@@ -31,5 +34,8 @@ if __name__ == '__main__':
         except Exception as e:
             print(e)
         quality = evaluate_policy(mdp, rtdp_g, ("1|1", "1|2"), ("0|0", "2|3"), 1000)
-        print_result_json(mdp, rtdp_g, ID, 4, (0.5, 0.8), ("1|1", "1|2"), ("0|0", "2|3"), vi_time, quality * 100, board)
-    collect_res_csv(f"results_{time.time()}.csv")
+        print_result_json(mdp, rtdp_g, ID, 4, (0.5, 0.8), ("1|1", "1|2"), ("0|0", "2|3"), vi_time, quality * 100,
+                      board, "RTDP", board_id)
+
+    local_time= time.localtime()
+    collect_res_csv(f"results_{local_time.tm_hour}{local_time.tm_min}{local_time.tm_sec}.csv")
